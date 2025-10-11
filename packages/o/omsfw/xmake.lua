c8a093e16b38313e3ffdf6c4dd8aacc2b9954364
Alexander Kozharsky <a.kozharsky@icloud.com>
2025-10-11 20:03:28 +0300

    Try to link omsfw with self.

--- packages/o/omsfw/xmake.lua
+++ packages/o/omsfw/xmake.lua
@@ -6,8 +6,8 @@ package('omsfw')
   add_versions('1.0.0', '7a002761f54fe7bcae57b2f2eb56961868ee229e')
 
   add_deps('objfw')
+  add_deps('omsfw')
 
   on_install(function (package)
-    package:add('links', {'objfw'})
     import('package.tools.xmake').install(package)
   end)
