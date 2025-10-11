399a408075a04e305646109a9c4bac3375a3d0e2
Alexander Kozharsky <a.kozharsky@icloud.com>
2025-10-11 19:24:37 +0300

    omsfw includes.

--- packages/o/omsfw/xmake.lua
+++ packages/o/omsfw/xmake.lua
@@ -5,10 +5,15 @@ package('omsfw')
   add_urls('https://github.com/kozharskyad/omsfw.git')
   add_versions('1.0.0', 'cde4aa5efb2ec11af3e60c19ae0c69926422915f')
 
+
+  on_load(function (package)
+    package:add('includedirs', 'include')
+  end)
+
   on_install(function (package)
-    local configs = {}
-    if package:config('static') then
-      configs.kind = 'static'
-    end
+    -- local configs = {}
+    -- if package:config('static') then
+    --   configs.kind = 'static'
+    -- end
     import('package.tools.xmake').install(package, configs)
   end)
