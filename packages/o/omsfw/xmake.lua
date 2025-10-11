4c961df00ec592122f89bdd2f091aa0995da652f
Alexander Kozharsky <a.kozharsky@icloud.com>
2025-10-11 20:50:25 +0300

    omsfw update.

--- packages/o/omsfw/xmake.lua
+++ packages/o/omsfw/xmake.lua
@@ -3,7 +3,7 @@ package('omsfw')
   set_license('LGPL-3.0')
 
   add_urls('https://github.com/kozharskyad/omsfw.git')
-  add_versions('1.0.0', 'c792d22ecb3b7cce4f41c3d4d01b4b3c1fe65018')
+  add_versions('1.0.0', '509015486ae25fafc54b1532c74958f4e9250772')
 
   add_deps('objfw')
   add_includedirs('include')
