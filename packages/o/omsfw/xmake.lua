29a3e257d00aaf4dddb0820e3c71245abc675d34
Alexander Kozharsky <a.kozharsky@icloud.com>
2025-10-12 12:51:44 +0300

    Return headers copying.

--- packages/o/omsfw/xmake.lua
+++ packages/o/omsfw/xmake.lua
@@ -3,11 +3,11 @@ package('omsfw')
   set_license('LGPL-3.0')
 
   add_urls('https://github.com/kozharskyad/omsfw.git')
-  add_versions('1.0.0', 'fb1647ae7f039e21a58fd9066e70a9212c39c27a')
+  add_versions('1.0.0', '6a5691da480bf7a8a2c671363120eb52ee4ab5a1')
 
   add_deps('objfw')
 
   on_install(function (package)
     import('package.tools.xmake').install(package)
-    -- os.cp('src/include', package:installdir())
+    os.cp('src/include', package:installdir())
   end)
